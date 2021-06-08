169058f63ceec5ae26493b130d34fd17ca094ec3
Antti Heroja <antti.heroja@gmail.com>
2021-06-08 16:17:52 +0300

    ci(xxx-xxx): ci pipeline new installment
    BREAKING CHANGE: not working previous API versions anymore

--- release.config.js
+++ release.config.js
@@ -1,13 +1,9 @@
 module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/AratioD/demo-github-actions",
-    "plugins": [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/npm",
-        "@semantic-release/exec",
-        ["@semantic-release/exec", {
-            "prepareCmd": "./update-version.sh ${nextRelease.version}",
-        }],
+    repositoryUrl: "git@github.com:AratioD/demo-github-actions.git",
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
     ]
 }
